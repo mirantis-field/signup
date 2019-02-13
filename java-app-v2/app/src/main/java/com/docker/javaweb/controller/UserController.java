3a67913e31b97d320af2487e5c9272451a63f0e8
Guillaume Morini <guillaume.morini@docker.com>
2019-02-13 12:26:13 +0100

    update HttpMessageConverter

--- java-app-v2/app/src/main/java/com/docker/javaweb/controller/UserController.java
+++ java-app-v2/app/src/main/java/com/docker/javaweb/controller/UserController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpEntity;
 import org.springframework.format.annotation.DateTimeFormat;
